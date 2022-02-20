ece7bd0d1ace9d912ef03479147dc73eb85c7b19
IvoCostaCunha <ivo.costacunha@icloud.com>
2022-02-20 20:44:40 +0100

    added json parse of nedded informations

--- TP2_IoT/tp2/src/main.cpp
+++ TP2_IoT/tp2/src/main.cpp
@@ -3,6 +3,8 @@
 #include "OneWire.h" 
 #include "DallasTemperature.h"
 
+#include <string>
+
 // D25 D26 for led
 // D23 for temperature sensor
 // D32 for luminosity sensor
@@ -26,35 +28,43 @@ void setup() {
 
 void loop() {
     int sensorValue = analogRead(A4); // Read pin 32 ADC1_4
-    Serial.print("photo-receptor : ");
-    Serial.println(sensorValue, DEC);
+    //Serial.print("photo-receptor : ");
+    //Serial.println(sensorValue, DEC);
 
     // Request a temperature mesure in C / O is the 1st sensor if there are more ...
     tempSensor.requestTemperaturesByIndex(0);
     // Read the last temperature in C (?) for sensor 0
     float temp = tempSensor.getTempCByIndex(0); 
-    Serial.print("Temp : ");
-    Serial.print(temp); 
-    Serial.println(" C");
+    //Serial.print("Temp : ");
+    //Serial.print(temp); 
+    //Serial.println(" C");
 
 
     // Check if led are on or off (HIGH or LOW)
-
-    Serial.print("Red led :");
+    String redLedStatus;
+    String greenLedStatus;
+    //Serial.print("Red led :");
     if(digitalRead(redLedPin == HIGH)) {
-        Serial.println("on");
+        //Serial.println("on");
+        redLedStatus = "on";
     }
     else {
-        Serial.println("off");
+        //Serial.println("off");
+        redLedStatus = "off";
     }
 
-    Serial.print("Green led : ");
+    //Serial.print("Green led : ");
     if(digitalRead(greenLedPin == HIGH)) {
-        Serial.println("on\n");
+        //Serial.println("on\n");
+        greenLedStatus = "on";
     }
     else {
-        Serial.println("off\n");
+        //Serial.println("off\n");
+        greenLedStatus = "off";
     }
 
+    String json = "{\n\"temperature\": \""+(String)temp+" C\",\n\"light\": \""+(String)sensorValue+" lm\",\n\"red-led\": \""+redLedStatus+"\",\n\"green-led\": \""+greenLedStatus+"\"\n}";
+
+    Serial.println(json+"\n");
     delay(1000);
 }
